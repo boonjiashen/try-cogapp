39b8437ee36aece46a8aa07bf883837db713c8f3
Jia-Shen Boon <jiashen@gmail.com>
2024-04-10 09:20:05 +0900

    Flesh out example CLI

--- src/try_cogapp/main.py
+++ src/try_cogapp/main.py
@@ -1,15 +1,27 @@
 import argparse
+import pprint
+
+import requests
+import logging
+
+logging.basicConfig()
+logger = logging.getLogger(__name__)
 
 
 def create_parser():
-    parser = argparse.ArgumentParser("My CLI")
-    parser.add_argument("--path", help="A useful path")
+    parser = argparse.ArgumentParser("URL-getter")
+    parser.add_argument("--url", default='https://api.github.com', help="URL to get")
 
     return parser
 
 
 def main():
-    print(create_parser().parse_args())
+    args = create_parser().parse_args()
+    logger.debug(args)
+
+    response = requests.get(args.url)
+    pprint.pprint(response.status_code)
+    pprint.pprint(response.json())
 
 
 if __name__ == "__main__":
